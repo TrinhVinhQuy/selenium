3fc932b69d86b1031889f25fbcae3cb854379cd0
TrinhVinhQuy <102006384+TrinhVinhQuy@users.noreply.github.com>
2023-10-14 14:33:54 +0700

    Add files via upload

---
+++ google-search.py
@@ -0,0 +1,14 @@
+# Max Base
+# https://github.com/BaseMax
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome()
+browser.get('https://www.google.com/')
+
+# document.querySelector("input.gLFyf.gsfi")
+search = browser.find_element_by_class_name('gLFyf')
+search.send_keys('BaseMax' + Keys.RETURN)
+print(browser.title)
+
+browser.quit()
